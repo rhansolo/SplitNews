b2b3b52e2ff945f66f54b73c0f228304f031b2b4
rhansolo <robinh@andrew.cmu.edu>
2020-06-20 22:16:27 -0400

    .

---
+++ web/react-flask-app/api/api.py
@@ -0,0 +1,15 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1> hello </h1>'
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
